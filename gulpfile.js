33fc39541d49882b56dba84518fd48fbcc772154
Konstantin <sayk.tiger09@gmail.com>
2022-03-26 10:40:16 +0300

    first screen

--- gulpfile.js
+++ gulpfile.js
@@ -34,10 +34,10 @@ gulp.task('styles',function(){
 
 gulp.task('watch', function(){
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'))
-    gulp.watch("src/*.html").on("change",gulp.parallel(`html`));
-    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
-    gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
-    gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
+    gulp.watch("src/*.html").on("change",gulp.parallel(`html`))
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'))
+    gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'))
+    gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'))
     gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
 });
 
