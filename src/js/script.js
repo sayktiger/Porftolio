56d694b7614cdc00a24cb2aa6c7f4345cdeb1917
Konstantin <sayk.tiger09@gmail.com>
2022-04-08 13:41:51 +0300

    correction script, link, text

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,13 @@
-const hamburger = document.querySelector(`.hamburger`),
+document.addEventListener("DOMContentLoaded", function() {
+
+	const hamburger = document.querySelector(`.hamburger`),
     menu = document.querySelector(`.menu`),
     close = document.querySelector(`.menu__close`),
-    overlay = document.querySelector(`.menu__overlay`);
+    overlay = document.querySelector(`.menu__overlay`),
+    link = document.querySelectorAll(`.menu__link`),
+    linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
+    V = 0.4; // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+
 
 hamburger.addEventListener(`click`, () => {
     menu.classList.add(`menu__active`);
@@ -15,6 +21,36 @@ overlay.addEventListener(`click`, () =>{
     menu.classList.remove(`menu__active`);
 });
 
+// link.addEventListener(`click`, () =>{
+//     menu.classList.remove(`menu__active`);
+// });
+link.forEach((item,i) => {
+    link[i].addEventListener(`click`, () =>{
+        menu.classList.remove(`menu__active`);
+    });
+});
+
+for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
+        e.preventDefault(); //отменяем стандартное поведение
+        var w = window.pageYOffset,  // производим прокрутка прокрутка
+            hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
+        t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+            start = null;
+        requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+        function step(time) {
+            if (start === null) start = time;
+            var progress = time - start,
+                r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+            window.scrollTo(0,r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash  // URL с хэшем
+            }
+        }
+    }, false);
+}
 
 
 const counters = document.querySelectorAll(`.skill__rating-item-counter`),
@@ -23,3 +59,6 @@ const counters = document.querySelectorAll(`.skill__rating-item-counter`),
 counters.forEach((item,i) =>{
     lines[i].style.width = item.innerHTML;
 });
+
+});
+
